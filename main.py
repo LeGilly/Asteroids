8676e734f17860adb2d033c8439a566f931b0ced
LeGilly <ggdude76@hotmail.com>
2025-06-24 22:42:42 +0200

    Adding classes for Asteroids, AsteroidField, CircleShape, Player

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import pygame
+from asteroid import Asteroid
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
+    dt = 0.0
+    clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) # Start at the center of the screen
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -13,8 +24,12 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0)) # Black background
+        screen.fill(BLACK_COLOR) # Black background
+        updatable.update(dt)
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000 # Convert milliseconds to seconds
 
 
 
